25c2615a7e4ee602584ea01b0eebd08b22b75bf4
Ankush2107 <ankush.kumar210701@gmail.com>
2023-08-28 17:28:02 +0530

    Creating Cuberto clone for learning purpose

---
+++ To_do_list2/script.js
@@ -0,0 +1,14 @@
+const taskInput = document.getElementById('taskInput');
+const addButton = document.getElementById('addButton');
+const taskList = document.getElementById('taskList');
+
+addButton.addEventListener('click', () => {
+  const taskText = taskInput.value;
+  if (taskText !== '') {
+    const li = document.createElement('li');
+    li.textContent = taskText;
+    taskList.appendChild(li);
+    taskInput.value = '';
+  }
+});
+
